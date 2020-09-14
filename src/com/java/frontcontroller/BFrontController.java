163ae9dfc7ac1aee6f690326be1af2ca1b872b41
이상현 <@10.71.208.53>
2020-09-14 17:53:47 +0900

    FrontController 작업중

---
+++ src/com/java/frontcontroller/BFrontController.java
@@ -0,0 +1,73 @@
+package com.java.frontcontroller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BFrontController
+ */
+@WebServlet("*.do")
+public class BFrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BFrontController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doGet");
+		actionDo(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doPost");
+		actionDo(request,response);
+	}
+	protected void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("actionDo");
+		
+		request.setCharacterEncoding("EUC-KR");
+		
+		String viewPage = null;
+		BCommand command = null;
+		
+		String uri = request.getRequestURI();
+		String conPath = request.getContextPath();
+		String com = uri.substring(conPath.length());
+		
+		if(com.equals("/write_view.do")) {
+			viewPage = "wirte_view.jsp";
+		}else if("/write.do") {
+			command = new BWriteCommand();
+			command.excute(request,response);
+			viewPage = "list.do";
+		}else if("/list.do") {
+			
+		}else if("/content_view.do") {
+			
+		}else if("/modify.do") {
+			
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+	}
+}
